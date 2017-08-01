37d5037dfde3b7ad560863bd60be93ac55c5b54f
rsawyer <rsawyer@tripwire.com>
2017-07-31 19:58:03 -0700

    Added initial contact probe model.

---
+++ models/ContactProbe.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * ContactProbe Model
+ * ==========
+ */
+
+var ContactProbe = new keystone.List('ContactProbe', {
+	track: {
+		createdBy: true,
+		updatedBy: true,
+		createdAt: true,
+		updatedAt: true
+	},
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'key', unique: true }
+});
+
+ContactProbe.add({
+  title: { type: Types.Text, required: true, index: true, initial: true },
+  heroImage: { type: Types.CloudinaryImage, collapse: true },
+	description: {
+		brief: { type: Types.Textarea, height: 150, initial: true },
+		extended: { type: Types.Textarea, height: 400 }
+	},
+	notes: { type: Types.Textarea, height: 150 },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'published', index: true }
+});
+
+ContactProbe.schema.virtual('description.full').get(function () {
+	return this.description.extended || this.description.brief;
+});
+
+ContactProbe.defaultColumns = 'title, state|20%, createdBy|20%, createdAt|20%';
+ContactProbe.register();
