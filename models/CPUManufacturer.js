a4090640329df5eb1dbff00ca7fbdc5be6f30209
rsawyer <rsawyer@tripwire.com>
2017-07-31 20:00:50 -0700

    Added CPU Manufacturer

--- models/CPUManufacturer.js
+++ models/CPUManufacturer.js
@@ -2,11 +2,11 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 /**
- * CPU Model
+ * CPUManufacturer Model
  * ==========
  */
 
-var CPU = new keystone.List('CPU', {
+var CPUManufacturer = new keystone.List('CPUManufacturer', {
 	track: {
 		createdBy: true,
 		updatedBy: true,
@@ -17,26 +17,25 @@ var CPU = new keystone.List('CPU', {
 	autokey: { path: 'slug', from: 'key', unique: true }
 });
 
-CPU.add({
+CPUManufacturer.add({
   title: { type: Types.Text, required: true, index: true, initial: true },
-	manufacturer: { type: Types.Relationship, ref: 'CPUManufacturer', many: false, createInline: true },
-	heroImage: { type: Types.CloudinaryImage, collapse: true },
+  heroImage: { type: Types.CloudinaryImage, collapse: true },
 	url: { type: Types.Url },
 	description: {
 		brief: { type: Types.Textarea, height: 150, initial: true },
 		extended: { type: Types.Textarea, height: 400 }
 	},
-	images: { type: Types.CloudinaryImages, collapse: true },
+  images: { type: Types.CloudinaryImages, collapse: true },
 	files: { type: Types.Relationship, ref:'DUTFile', many: true, createInline: true },
 	notes: { type: Types.Textarea, height: 150 },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'published', index: true }
 });
 
-CPU.relationship({ ref: 'Microcontroller', path: 'microcontrollers', refPath: 'cpus' });
-
-CPU.schema.virtual('description.full').get(function () {
+CPUManufacturer.schema.virtual('description.full').get(function () {
 	return this.description.extended || this.description.brief;
 });
 
-CPU.defaultColumns = 'title, state|20%, createdBy|20%, createdAt|20%';
-CPU.register();
+CPUManufacturer.relationship({ ref: 'CPU', path: 'cpus', refPath: 'manufacturer' });
+
+CPUManufacturer.defaultColumns = 'title, state|20%, createdBy|20%, createdAt|20%';
+CPUManufacturer.register();
